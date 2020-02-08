8b2cd211ebcfda8b588257b367cd91a14847c8c9
LightDiscord <root@arnaud.sh>
2020-02-08 21:39:50 +0100

    feat: add i3wm config

--- default.nix
+++ default.nix
@@ -6,20 +6,27 @@ let
   alacritty = import configs/alacritty;
   htop = import configs/htop;
   rofi = import configs/rofi;
+  i3wm = import configs/i3wm;
 in {
   nixpkgs.overlays = import ./overlays;
 
   fonts.fontconfig.enable = true;
 
+  home.keyboard.layout = "fr";
+
+  xsession.enable = true;
+  xsession.windowManager.command = "${pkgs.i3}/bin/i3";
+
   home.packages =
     builtins.concatMap
-      (packages: pkgs.callPackage packages {})
+      (module: pkgs.callPackage module.packages {})
       [
-        alacritty.packages
-        htop.packages
-        rofi.packages
+        alacritty
+        htop
+        rofi
+        i3wm
       ]
-    ++ (with pkgs; [ gnome3.adwaita-icon-theme hicolor-icon-theme ]);
+    ++ (with pkgs; [ gnome3.adwaita-icon-theme hicolor-icon-theme i3 ]);
 
     home.file = let
       mapFiles = map (mapAttrs (_: source: { inherit source; }));
@@ -27,6 +34,7 @@ in {
       alacritty.files
       htop.files
       rofi.files
+      i3wm.files
     ]);
 
   gtk.enable = true;
